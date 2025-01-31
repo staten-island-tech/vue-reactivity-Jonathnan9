e5cde4353e67d1fac61d3bec6f9210b7849c0f52
Jonathan <158211587+Jonathan9123@users.noreply.github.com>
2025-01-31 02:14:30 -0500

    ae

--- app/src/App.vue
+++ app/src/App.vue
@@ -15,10 +15,6 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-    <div class="container mx-auto p-4">
-      <h1 class="text-3xl font-bold text-center mb-4">Hello Vite!</h1>
-      <button class="btn btn-primary">Click Me</button>
-    </div>
   </header>
 
   <RouterView />
