ef63b559ff5486668c032121d6fc292b0217a638
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2025-02-10 12:31:34 -0500

    lmhhjhj

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
+import CreateScore from './components/CreateScore.vue'
 </script>
 
 <template>
   <div class="flex">
     <NavBar />
+    <CreateScore />
     <RouterView />
   </div>
 </template>
