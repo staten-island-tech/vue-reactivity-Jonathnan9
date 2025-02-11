1ff1fec972175507b7e9cc66a13f8789847b6182
Jonathan <158211587+Jonathnan9@users.noreply.github.com>
2025-02-11 12:20:01 -0500

    u

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
-import CreateScore from './components/CreateScore.vue'
+import CreateScore from './components/ProductCart.vue'
 </script>
 
 <template>
